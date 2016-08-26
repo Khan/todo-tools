ea3c22d52f4f7f81042046f7de3dc1061ca59a5c
William Farmer <willzfarmer@gmail.com>
2016-08-25 19:46:43 -0700

    Updated code from review

--- setup.py
+++ setup.py
@@ -1,40 +1,11 @@
 #!/usr/bin/env python
 
-# Custom install process
-# https://blog.niteoweb.com/setuptools-run-custom-code-in-setup-py/
-
 from setuptools import setup
-from setuptools.command.install import install
-
-import os
-import re
-
-
-class CustomInstallCommand(install):
-    """Customized setuptools install command - Adds todo to .bashrc """
-    def run(self):
-        bashrcfilename = os.path.join(os.path.expanduser('~/.bashrc'))
-        already_installed = False
-        if os.path.isfile(bashrcfilename):
-            with open(bashrcfilename, 'r') as bashrcfile:
-                for line in bashrcfile.readlines():
-                    if re.search('todo -c', line):
-                        already_installed = True
-        else:
-            already_installed = False
-        if not already_installed:
-            with open(bashrcfilename, 'a') as bashrcfile:
-                bashrcfile.write('\ntodo -c\n')
-
-        install.run(self)
 
 
 setup(name='TODO-Tools',
         version='1.0',
         description='Tools to manage git TODOs',
-        license='Apache',
+        license='MIT',
         packages=['todo_tools'],
-        scripts=['bin/todo'],
-        cmdclass={
-            'install': CustomInstallCommand,
-        })
+        scripts=['bin/todo'])
